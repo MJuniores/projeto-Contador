6234c3ea1ac0750e15aaabe2eaac3bae3229cb0d
MJuniores <128878668+MJuniores@users.noreply.github.com>
2024-05-04 22:45:14 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Primeiro valor");
+        int valorUm = sc.nextInt();
+
+        System.out.println("Segundo valor");
+        int valorDois = sc.nextInt();
+
+        if (valorUm > valorDois){
+            throw new ParametroInvalido("O segundo valor deve ser maior que o primeiro");
+        }
+
+        int diferença = valorDois - valorUm;
+
+        for (int i = 1; i <= diferença; i++){
+            System.out.println("Imprimindo o número "+ i);
+        }
+    }
+}
+
+    class ParametroInvalido extends RuntimeException{
+    public ParametroInvalido(String message){
+        super(message);
+    }
+    }
